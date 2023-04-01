851e8796ad0c86a0d9cd329601eb19bd4710d970
Zachary <zachary@zipcoders-mbp-2.mynetworksettings.com>
2023-04-01 11:24:38 -0400

    dog test done

--- src/test/java/io/zipcoder/polymorphism/TestDog.java
+++ src/test/java/io/zipcoder/polymorphism/TestDog.java
@@ -1,7 +1,31 @@
 package io.zipcoder.polymorphism;
+import org.junit.Assert;
 import org.junit.Test;
 public class TestDog {
 
+    @Test
+    public void testSpeak(){
+        String expected = "Bark Bark!";
+        Dog dog = new Dog("AirBud", 5);
+        String actual = dog.speak();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDogConstructor(){
+        int expectedAge = 2;
+        String expectedName = "AirBud";
+
+        Dog dog = new Dog("AirBud", 5);
+
+        String actualName = dog.getName();
+        int actualAge = dog.getAge;
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+
+    }
 }
 
 
