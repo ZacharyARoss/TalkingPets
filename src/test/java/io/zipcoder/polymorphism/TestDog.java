f6f1ed714b4be20ff896392a1e9750da94c82632
Zachary <zachary@zipcoders-mbp-2.mynetworksettings.com>
2023-04-01 11:40:18 -0400

    changed age dog class

--- src/test/java/io/zipcoder/polymorphism/TestDog.java
+++ src/test/java/io/zipcoder/polymorphism/TestDog.java
@@ -6,7 +6,7 @@ public class TestDog {
     @Test
     public void testSpeak(){
         String expected = "Bark Bark!";
-        Dog dog = new Dog("AirBud", 5);
+        Dog dog = new Dog("AirBud", 2);
         String actual = dog.speak();
 
         Assert.assertEquals(expected, actual);
@@ -17,7 +17,7 @@ public class TestDog {
         int expectedAge = 2;
         String expectedName = "AirBud";
 
-        Dog dog = new Dog("AirBud", 5);
+        Dog dog = new Dog("AirBud", 2;
 
         String actualName = dog.getName();
         int actualAge = dog.getAge;
